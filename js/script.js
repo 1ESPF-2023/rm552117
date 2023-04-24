1e785d658059cb0b5ad8b23d9ce9498ee1f222bc
MuriloEloys <rm552117@fiap.com.br>
2023-04-24 09:55:19 -0300

    Exercício não ponto

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //Recuperando o botão com o "get element by id"
-const botao = document.getElementById("btn 1")
+//const botao = document.getElementById("btn 1")
 
 //recuperar um item específico com a função query selector
 // const seletor = document.querySelector("li")
@@ -7,11 +7,11 @@ const botao = document.getElementById("btn 1")
 
 //Recuperar uma lista de elementos com ma função com querry selector on
 
-const seletores = document.querySelectorAll("li");
+//const seletores = document.querySelectorAll("li");
 
-seletores.forEach((item)=>{
-    console.log('Items selecionados : $'(item.textContent))
-})
+//seletores.forEach((item)=>{
+//    console.log('Items selecionados : $'(item.textContent))
+//})
 
 //setTimePut
 //setInterval
@@ -19,3 +19,29 @@ seletores.forEach((item)=>{
 //Math.Cell
 //Math.Random
 //math.floon
+
+function mudacor(){
+    let r=0;
+    let g=0;
+    let b=0;
+
+    r= Math.ceil(Math.random() *255);
+    g= Math.ceil(Math.random() *255);
+    b= Math.ceil(Math.random() *255);
+    
+    const cabecalho = document.querySelector('.cabecalho');
+    cabecalho.style = `../img/banner-1440x300-1.jpg`
+    const banner1 = document.querySelector('.banner1');
+    banner1.style = `(./img/banner-lateral-1.png)`;
+    const banner2 = document.querySelector('.banner2');
+    banner2.style.backgroundColor = `rgb(${r},${g},${b})`;
+    const rodape = document.querySelector('.rodape');
+    rodape.style.backgroundColor = `rgb(${r},${g},${b})`;
+    const conteudo = document.querySelector('.conteudo');
+    conteudo.style.backgroundColor = `rgb(${r},${g},${b})`;
+    
+    tmp = setTimeout(mudacor, 1000, )
+
+
+}
+mudacor();

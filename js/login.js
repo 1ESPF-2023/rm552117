630c858691f174163030a4069f36a0c235855e5f
MuriloEloys <rm552117@fiap.com.br>
2023-05-22 09:04:51 -0300

    Validação

--- js/login.js
+++ js/login.js
@@ -6,19 +6,42 @@ let senha = "";
 const botaoSubmit = document.querySelector("#btnSubmit");
 
 botaoSubmit.addEventListener("click",()=>{
-
+    
     const inputUser = document.querySelector("input[name='nmUser']");
     const inputPass = document.querySelector("input[name='pass']");
     const labelUser = document.querySelector("label[for='idUser']");
     const labelPass = document.querySelector("label[for='idPass']");
+    
+    
+    const usuarioLogado ={
+        nomeUsuarioLogado:inputUser.value,
+        senhaUsuarioLogado:inputPass.value
+    }
 
-
+    
     if(inputUser.value.length > 3 && inputPass.value.length > 3){
         inputUser.setAttribute("style", "outline-color:#00ff00;")
         inputPass.setAttribute("style", "outline-color:#00ff00;")
         labelUser.setAttribute("style","color:#00ff00;");
         labelPass.setAttribute("style","color:#00ff00;");
+        if(usuarioLogado.nomeUsuarioLogado == "tutu" && usuarioLogado.senhaUsuarioLogado == "123456"){
+            alert("Válido")
+        }
+        else{
+            alert("Não válido")
+        }
+    }
+    else{
+        inputUser.setAttribute("style", "outline-color:#ff0000;")
+        inputPass.setAttribute("style", "outline-color:#ff0000;")
+        labelUser.setAttribute("style","color:#ff0000;");
+        labelPass.setAttribute("style","color:#ff0000;");
     }
 
+    addEventListener("click", (evt)=>{
+        evt.target.id=botaoSubmit(
+            console.log("Achei o botão")
+        )
+    })
 
 });
